db78ddee32e8ec0e6d9971197389ea9d4ba1d63f
ThadsaneeIADT <n00192658@iadt.ie>
2022-04-04 17:27:50 +0100

    ca2

---
+++ CA2.py
@@ -0,0 +1,86 @@
+#CA2
+
+from mimetypes import init
+import cv2
+from cv2 import threshold
+import numpy as np
+
+
+########################################################################
+webCamFeed = True  # set to false if no webcam available
+pathImage = "Imagepi\\image004.jpg"
+# main webcam -> 0
+cap = cv2.VideoCapture(0)
+cap.set(10, 160)
+heightImg = 640
+widthImg = 480
+########################################################################
+
+count = 0
+
+# function to initialize Trackbars
+def nothing(x):
+    pass
+
+def initializeTrackbars(initialTrackbarVal=0):
+    cv2.namedWindow("Trackbars")
+    cv2.resizeWindow("Trackbars", 360, 240)
+    cv2.createTrackbar("Threshold1", "Trackbars", initialTrackbarVal, 255, nothing)
+    cv2.createTrackbar("Threshold2", "Trackbars", initialTrackbarVal, 255, nothing)
+
+def valTrackbars():
+    Threshold1 = cv2.getTrackbarPos("Threshold1", "Trackbars")
+    Threshold2 = cv2.getTrackbarPos("Threshold2", "Trackbars")
+    src = Threshold1, Threshold2
+    return src 
+
+while True:
+    # input is either webcam or image
+    if webCamFeed:
+        success, img = cap.read()
+    else:
+        img = cv2.imread(pathImage)
+    # RESIZE IMAGE
+    img = cv2.resize(img, (widthImg, heightImg))
+    # CREATE A BLANK IMAGE FOR TESTING DEBUGING IF REQUIRED
+    imgBlank = np.zeros((heightImg, widthImg, 3), np.uint8)
+    # CONVERT IMAGE TO GRAY SCALE
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    imgBlur = cv2.GaussianBlur(imgGray, (5, 5), 1)  # ADD GAUSSIAN BLUR
+    thres = valTrackbars()  # GET TRACK BAR VALUES FOR THRESHOLDS
+    imgCanny = cv2.Canny(imgBlur, thres[0], thres[1])  # APPLY CANNY BLUR
+    kernel = np.ones((5, 5))
+    imgDial = cv2.dilate(imgCanny, kernel, iterations=2)  # APPLY DILATION
+    imgThreshold = cv2.erode(imgDial, kernel, iterations=1)  # APPLY EROSION
+
+
+    imgFinal = imgCanny
+    cv2.imshow("1. Original", img)
+    cv2.imshow("2. Grayscale", imgGray)
+    cv2.imshow("3. Blur", imgBlur)
+    cv2.imshow("4. Canny", imgCanny)
+    cv2.imshow("5. Dilate", imgDial)
+    cv2.imshow("6. Treshold", imgThreshold)
+    #cv2.imshow("7. imgContours", imgContours)
+
+#user interaction 
+    # Press x  on keyboard to  exit
+    # Close and break the loop after pressing "x" key
+    if cv2.waitKey(1) & 0XFF == ord('x'):
+        break  # exit infinite loop
+
+     # SAVE IMAGE WHEN 's' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('s'):
+        print("saving")
+        # save image to folder using cv2.imwrite()
+        cv2.imwrite("Scanned/myImage"+str(count)+".jpg", imgFinal)
+        cv2.waitKey(300)
+        count += 1
+
+
+# When everything done, release
+# the video capture object
+cap.release()
+
+# Closes all the frames
+cv2.destroyAllWindows()
